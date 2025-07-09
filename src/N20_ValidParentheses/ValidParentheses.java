62cd3e2687e6e4775de0ad9ede03b98796f23705
Ymid Ortega <yortegap7920@gmail.com>
2025-07-08 19:15:56 -0500

    feat(leetcode): add solution for Valid Parentheses.

---
+++ src/N20_ValidParentheses/ValidParentheses.java
@@ -0,0 +1,24 @@
+package N20_ValidParentheses;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> chars = new Stack();
+        for (char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                chars.push(c);
+            }else{
+                if(chars.isEmpty()) return false;
+                char popped = chars.pop();
+                if(c == ')' && popped != '(') return false;
+                if(c == '}' && popped != '{') return false;
+                if(c == ']' && popped != '[') return false;
+            }
+        }
+        return chars.isEmpty();
+    }
+}
