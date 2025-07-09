6313596174394c1f5e14a63831a65135f5be34e8
Ymid Ortega <yortegap7920@gmail.com>
2025-07-08 19:15:22 -0500

    feat(leetcode): add solution for Roman to Integer.

---
+++ src/N13_RomanToInterger/RomanToInterger.java
@@ -0,0 +1,43 @@
+package N13_RomanToInterger;
+
+public class RomanToInterger {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = getRomanValue(c);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+            prevValue = value;
+        }
+
+        return result;
+    }
+
+    private int getRomanValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Invalid Roman numeral: " + c);
+        }
+    }
+}
