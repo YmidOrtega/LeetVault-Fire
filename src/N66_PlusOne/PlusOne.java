7dbfbfa2df820c7e505e72e8952d041c377f610c
Ymid Ortega <yortegap7920@gmail.com>
2025-07-16 01:02:41 -0500

    feat: Add LeetCode Plus One problem solution

---
+++ src/N66_PlusOne/PlusOne.java
@@ -0,0 +1,41 @@
+package N66_PlusOne;
+
+import java.math.BigInteger;
+
+public class PlusOne {
+    // SOLUTION REAL.
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    // SOLUTION FOR SMALL NUMBERS.
+    public int[] plusOne1(int[] digits) {
+        if (digits.length == 1 && digits[digits.length-1] <= 8){
+            int[] result = new int[digits.length];
+            result[0] = digits[0] + 1 ;
+            return result;
+        }else {
+            String num ="";
+            for (int i = 0; i <= digits.length-1; i++){
+                num += String.valueOf(digits[i]);
+            }
+            int incrementNum = Integer.valueOf(num) + 1;
+            String incrementNumString = String.valueOf(incrementNum);
+            int[] result = new int[incrementNumString.length()];
+            for (int j = 0; j <= incrementNumString.length()-1 ; j ++){
+                result[j] = Integer.valueOf(String.valueOf(incrementNumString.charAt(j)));
+
+            }
+            return result;
+        }
+    }
+}
+
