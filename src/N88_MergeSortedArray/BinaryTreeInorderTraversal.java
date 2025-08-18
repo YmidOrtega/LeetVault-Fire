cdd4d73c96a9384097a7584fb0796e203a04f455
Ymid Ortega <yortegap7920@gmail.com>
2025-08-17 23:32:09 -0500

    feat(leetcode): add solution for Binary Tree Inorder Traversal.

---
+++ src/N88_MergeSortedArray/BinaryTreeInorderTraversal.java
@@ -0,0 +1,39 @@
+package N88_MergeSortedArray;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+        }
+    }
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList();
+	    Stack<TreeNode> stack = new Stack();
+	    TreeNode cur = root;
+	    while(cur != null || !stack.isEmpty()){
+		    while(cur != null){
+			    stack.push(cur);
+			    cur = cur.left;
+            }
+		    cur = stack.pop();
+		    list.add(cur.val);
+		    cur = cur.right;
+        }
+        return list;
+    }
+}
