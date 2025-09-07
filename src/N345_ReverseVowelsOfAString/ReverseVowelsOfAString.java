77fc0ce9a6008814873f106b62bd840b7dd6eafa
Ymid <yortegap7920@gmail.com>
2025-09-07 18:01:05 -0500

    feat(leetcode): add solution for Reverse Vowels of a String.

---
+++ src/N345_ReverseVowelsOfAString/ReverseVowelsOfAString.java
@@ -0,0 +1,29 @@
+package N345_ReverseVowelsOfAString;
+
+import java.util.ArrayList;
+
+public class ReverseVowelsOfAString {
+    public String reverseVowels(String s) {
+        int l = 0, r = s.length() - 1, length = s.length();
+        char ch[] = s.toCharArray();
+        while (l < r) {
+            if(!isVowel(ch[l])) l++;
+            else if(!isVowel(ch[r])) r--;
+            else {
+                char temp = ch[l];
+                ch[l] = ch[r];
+                ch[r] = temp;
+                l++;
+                r--;
+            }        
+        }
+        return String.valueOf(ch);
+    }
+
+    public static boolean isVowel (char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            return true;
+        }
+        return false;
+    }
+}
