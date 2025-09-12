9a9bc71bd4ffbe266a25add8c92c44945fc094d7
Ymid <yortegap7920@gmail.com>
2025-09-12 17:32:35 -0500

    feat(leetcode): add solution for Product of Array Except Self.

--- src/N151_ReverseWordsInAString/ReverseWordsInAString.java
+++ src/N151_ReverseWordsInAString/ReverseWordsInAString.java
@@ -2,9 +2,28 @@ package N151_ReverseWordsInAString;
 
 public class ReverseWordsInAString {
     public String reverseWords(String s) {
-        String newString = s.trim();
-        for (int i = newString.length() - 1; i >= 0; i--){
-            
+        if(s == null) return null;
+        char[] charArray = s.toCharArray();
+        char[] revSt = new char[s.length()];
+        int ind = reverse(charArray,revSt,0);
+        return new String(revSt,0,ind);
+    }
+    public int reverse(char[] ch,char[] rev,int start){
+        while(start < ch.length && ch[start] == ' ') {
+            start++;
         }
+        int end = start;
+        while(end<ch.length && ch[end] != ' ') {
+            end++;
+        }
+        if(start == end) return 0;
+        int len = reverse(ch,rev,end);
+        if(len != 0) {
+            rev[len++] = ' ';
+        }
+        while(start < end) {
+            rev[len++] = ch[start++];
+        }
+        return len;
     }
 }
