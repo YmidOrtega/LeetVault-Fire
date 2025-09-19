77c73b16ba7ec42ddd3716e5e9c7319944302449
Ymid <yortegap7920@gmail.com>
2025-09-18 21:36:30 -0500

    feat(leetcode): add solution for Max Consecutive Ones III.

--- src/N1004_MaxConsecutiveIII/MaxConsecutiveIII.java
+++ src/N1004_MaxConsecutiveIII/MaxConsecutiveIII.java
@@ -2,15 +2,19 @@ package N1004_MaxConsecutiveIII;
 
 public class MaxConsecutiveIII {
     public int longestOnes(int[] nums, int k) {
-        int maxNums = 0, l = 0, r = 0;
-        while (l < nums.length){
-            if(nums[l] == 0) k--;
-            if(k < 0){
-                if(nums[r] == 0)k++;
-                r++;
+        int left = 0, right = 0, maxOnes = 0, zeroCount = 0;
+        for (right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
             }
-            l++;
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxOnes = Math.max(maxOnes, right - left + 1);
         }
-        return l-r;
+        return maxOnes;
     }
 }
