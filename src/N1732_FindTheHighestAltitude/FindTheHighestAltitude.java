dd0f8f94a7c0fd44c4dc39bfbfe098208c3ca95f
Ymid <yortegap7920@gmail.com>
2025-09-19 23:43:53 -0500

    feat(leetcode): add solution for Find the Highest Altitude.

---
+++ src/N1732_FindTheHighestAltitude/FindTheHighestAltitude.java
@@ -0,0 +1,13 @@
+package N1732_FindTheHighestAltitude;
+
+public class FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+        return maxAltitude;
+    }
+}
