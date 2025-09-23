3258ca708fd37ab3cea627a3d7e8bf387913f779
Ymid <yortegap7920@gmail.com>
2025-09-22 23:18:07 -0500

    feat(leetcode): add solution for Unique Number of Occurrences.

--- src/N1207_UniqueNumber0fOccurrences/UniqueNumberOfOcurrences.java
+++ src/N1207_UniqueNumber0fOccurrences/UniqueNumberOfOcurrences.java
@@ -33,14 +33,12 @@ public class UniqueNumberOfOcurrences {
     public boolean uniqueOccurrences2(int[] arr) {
         Map<Integer, Integer> freq = new HashMap<>();
         for (int x : arr) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
+            freq.put(x, freq.getOrDefault(x,  0) + 1);
         }
-
         Set<Integer> s = new HashSet<>();
         for (int x : freq.values()) {
             s.add(x);
         }
-
         return freq.size() == s.size();
     }
 }
