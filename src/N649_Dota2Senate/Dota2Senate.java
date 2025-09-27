d031169a5ee0eaffd56a1970011e12d9c7e80c3e
Ymid <yortegap7920@gmail.com>
2025-09-26 23:54:14 -0500

    feat(leetcode): add solution for Dota2 Senate.

---
+++ src/N649_Dota2Senate/Dota2Senate.java
@@ -0,0 +1,23 @@
+package N649_Dota2Senate;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Dota2Senate {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qd = new LinkedList<>();
+        int n = senate.length();
+        for(int i = 0;i < n;i++){
+            if(senate.charAt(i) == 'R') qr.add(i);
+            else qd.add(i);
+        }
+        for(;!qr.isEmpty() && !qd.isEmpty();){
+            int r_i = qr.poll();
+            int d_i = qd.poll();
+            if(r_i < d_i) qr.add(r_i + n);
+            else qd.add(d_i + n);
+        }
+        return qr.size() > qd.size() ? "Radiant" : "Dire";
+    }
+}
