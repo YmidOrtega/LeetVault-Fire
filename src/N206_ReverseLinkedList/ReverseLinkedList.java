7c7f156c0994116863ebc74920a9b4da562d1b21
Ymid <yortegap7920@gmail.com>
2025-09-30 22:41:23 -0500

    refactor(leetcode): update Reverse Linked List solution.

--- src/N206_ReverseLinkedList/ReverseLinkedList.java
+++ src/N206_ReverseLinkedList/ReverseLinkedList.java
@@ -1,5 +1,23 @@
 package N206_ReverseLinkedList;
 
 public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
+    public ListNode reverseList(ListNode head) {
+        if(head == null ||head.next == null) return head;
+        ListNode node = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+        return node; 
+    }
 }
