c9038d329a0aa36703c67cf4c370ce05c7f5e002
Ymid <yortegap7920@gmail.com>
2025-09-30 23:35:11 -0500

    feat(leetcode): add solution for Contains Duplicate II.

---
+++ src/N219_ContainsDuplicateII/ContainsDuplicateII.java
@@ -0,0 +1,18 @@
+package N219_ContainsDuplicateII;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
+            }
+            seen.put(val, i);
+        }
+        return false;        
+    }
+}
