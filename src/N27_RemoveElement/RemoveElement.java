c83ee24131234ceded1d6689cf0f33b35f6a4133
Ymid <yortegap7920@gmail.com>
2025-10-02 23:48:04 -0500

    feat(leetcode): add solution for Remove Element.

--- src/N27_RemoveElement/RemoveElement.java
+++ src/N27_RemoveElement/RemoveElement.java
@@ -3,10 +3,8 @@ package N27_RemoveElement;
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int x = nums.length;
-
         if(x == 1 && nums[x-1] == val) return 0;
         int j = 0;
-
         for (int i = 0; i < x; i++){
             if(nums[i] != val){
                 nums[j] = nums[i];
@@ -14,6 +12,5 @@ public class RemoveElement {
             }
         }
         return j;
-
     }
 }
