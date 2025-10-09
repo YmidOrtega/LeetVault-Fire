0e35cea7c8f0e8b34567b80db6bce79a041bef47
Ymid <yortegap7920@gmail.com>
2025-10-08 22:20:00 -0500

    feat(leetcode): add solution for Power of Two.

--- src/N231_PowerOfTwo/PowerOfTwo.java
+++ src/N231_PowerOfTwo/PowerOfTwo.java
@@ -4,11 +4,4 @@ public class PowerOfTwo {
     public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
     }
-
-    public static void main(String[] args) {
-        PowerOfTwo powerOfTwo = new PowerOfTwo();
-        System.out.println(powerOfTwo.isPowerOfTwo(15));
-    }
-
-
 }
