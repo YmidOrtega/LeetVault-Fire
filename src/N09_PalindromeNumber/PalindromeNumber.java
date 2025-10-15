c77f3f96e8c7f15495f52d1b7fbd3bb8704a3d87
Ymidツ <yortegap7920@gmail.com>
2025-10-14 23:07:50 -0500

    feat(leetcode): add solution for Palindrome Number.

--- src/N09_PalindromeNumber/PalindromeNumber.java
+++ src/N09_PalindromeNumber/PalindromeNumber.java
@@ -2,20 +2,15 @@ package N09_PalindromeNumber;
 
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-       if (0 > x){
-           return false;
-       }
-       String str = Integer.toString(x);
-       int left = 0;
-       int right = str.length() - 1;
-       while (left < right){
-           if(str.charAt(left) != str.charAt(right)){
-               return false;
-           }
-           left++;
-           right--;
-
-       }
+        if (0 > x) return false;
+        String str = Integer.toString(x);
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right){
+            if(str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
         return true;
     }
 
