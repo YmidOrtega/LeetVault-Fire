38b972c577869ed02fb087cad0a86c36df56f073
Ymidツ <yortegap7920@gmail.com>
2025-10-15 19:56:46 -0500

    feat(leetcode): add solution for Reverse Integer

---
+++ src/N07_ReverseInteger/ReverseInteger.java
@@ -0,0 +1,14 @@
+package N7_ReverseInteger;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int num = Math.abs(x), rev = 0;
+        while (num != 0) {
+            int ld = num % 10;
+            if (rev > (Integer.MAX_VALUE - ld) / 10) return 0;
+            rev = rev * 10 + ld;
+            num = num / 10;
+        }
+        return (x < 0) ? (-rev) : rev;
+    }
+}
