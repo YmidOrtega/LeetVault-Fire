7f8f2ae1332df454a94e6788d7c45e9e6029f279
Ymidツ <yortegap7920@gmail.com>
2025-10-15 20:30:01 -0500

    feat(leetcode): add solution for Happy Number.

--- src/N202_HappyNumber/HappyNumber.java
+++ src/N202_HappyNumber/HappyNumber.java
@@ -1,17 +1,22 @@
 package N202_HappyNumber
 
 public class HappyNumber{
-    public boolean isHappy(int n) {
-        if(n == 1 || n == 7) return true;
-        else if(n < 10) return false;
-        else{
-            int sum = 0;
-            while(n > 0){
-                int temp = n % 10;
-                sum += temp * temp;
-                n = n / 10;
-            }
-            return isHappy(sum);
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        int slow = n, fast = getNext(n); 
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        return fast == 1;
     }
 }
